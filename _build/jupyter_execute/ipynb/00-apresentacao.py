2c613f5f4ebc1acbbc562c96561ca54f94eccb4c
Gustavo Oliveira <gustavo.oliveira@ci.ufpb.br>
2022-11-29 17:38:27 -0300

    Uping preview of lecture 3

--- _build/jupyter_execute/ipynb/00-apresentacao.py
+++ _build/jupyter_execute/ipynb/00-apresentacao.py
@@ -61,13 +61,12 @@ display(HTML('course-treemap.html'))
 
 # ## Conteúdo 
 # 
-# 
-# 
 # ### Módulo 1: Fundamentos (20h)
 # 
 # - Conteúdo (18h)
 #     - [Introdução à Visualização de Dados](01a-introducao.ipynb) (1h)
 #     - [Marcos Históricos da Visualização de Dados](01b-historico.ipynb) (1h)
+#     - [Criação de Representações Visuais e Estética](01b-historico.ipynb) (2h)
 # - Avaliação (2h)
 #     - Relatório de estudo de caso 1 (2h)
 # 
