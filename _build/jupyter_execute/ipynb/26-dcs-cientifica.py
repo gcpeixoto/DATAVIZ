9eb1c7f2aa5a7cb8aa5d55cbfbbff2393fb6d24b
Gustavo Oliveira <gustavo.oliveira@ci.ufpb.br>
2023-05-03 13:59:17 -0300

    Uping DCS for sciviz. Needs improvement and focus on specific applications later on. Currently based only on Paraview guides.

---
+++ _build/jupyter_execute/ipynb/26-dcs-cientifica.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # _Dataviz Code Session_: Visualização Científica
+
+# ## Objetivos da DCS
+# 
+# - Aplicar técnicas de dataviz para plotagem e manipulação de representações visuais científicas;
+# - Manipular representações visuais multidimensionais, filtros sobre dados e animações;
+
+# ## Ferramentas utilizadas
+# 
+# - Software
+#     - [Paraview](https://www.paraview.org)
+
+# ## Aplicação do modelo referencial
+# 
+# - Vide {numref}`Capítulo %s <cap:estetica>`.
+
+# Aplicação _hands-on_.
+
+# ## Recursos de aprendizagem
+# 
+# - Materiais disponíveis [aqui](https://www.paraview.org/resources/)
